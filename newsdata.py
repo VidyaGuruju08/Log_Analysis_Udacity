91ef45bf1b701a6f65d5b4acadbfde95d32a9132
VidyaGuruju08 <45555846+VidyaGuruju08@users.noreply.github.com>
2019-01-23 21:24:32 +0530

    Update newsdata.py

--- newsdata.py
+++ newsdata.py
@@ -1,24 +1,25 @@
-#!/usr/bin/env python3
+##!/usr/bin/env python
 import psycopg2
 
-#The most popular three articles of all time
+# The most popular three articles of all time
 query_articles = ("""create view pop1_articles as select title,
                     count(title) as views from articles,
                     log where log.path = concat('/article/',articles.slug)
                     group by title order by views desc limit 3""")
-#The most popular three article authors of all time
+# The most popular three article authors of all time
 query_authors = ("""create view pop1_authors as select name,
                     count(name) as views from articles,authors,
                     log where log.path = concat('/article/',articles.slug)
                     and articles.author=authors.id group by name order by
                     views desc limit 3""")
-#On which days did more than 1% of requests lead to errors
+# On which days did more than 1% of requests lead to errors
 query_errors = ("""create view pop_errors as select date(time),
                     round(100.0*sum(case log.status when '200 OK'
                     then 0 else 1 end)/count(log.status),2)as percent_error
                     from log group by date(time) order by percent_error""")
 
-#def for popular articles
+
+# def for popular articles
 def popular_articles_inDB(query_articles):
     db = psycopg2.connect(dbname="news")
     cur = db.cursor()
@@ -31,7 +32,8 @@ def popular_articles_inDB(query_articles):
         print(str(i[0])+"  --"+str(i[1])+"views")
     return
 
-#def for popular articles authors
+
+# def for popular articles authors
 def popular_authors_inDB(query_authors):
     db = psycopg2.connect(dbname="news")
     cur = db.cursor()
@@ -44,7 +46,8 @@ def popular_authors_inDB(query_authors):
         print(str(i[0])+" \t--"+str(i[1])+"views")
     return
 
-#def for errors more than 1%
+
+# def for errors more than 1%
 def error_percent_inDB(query_errors):
     db = psycopg2.connect(dbname="news")
     cur = db.cursor()
